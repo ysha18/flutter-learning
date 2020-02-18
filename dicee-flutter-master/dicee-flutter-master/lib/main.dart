49317af1036964c343c847633e85dfc4ccc70b27
Harold Yeo <haroldyeo@gmail.com>
2020-02-17 23:52:46 -0600

    dice app on going

---
+++ dicee-flutter-master/dicee-flutter-master/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('Left button pressed');
+              },
+                child: Image.asset('images/dice1.png')
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onLongPress: (){
+                  print('That was a long press on the right button');
+                },
+                child: Image.asset('images/dice1.png')
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
